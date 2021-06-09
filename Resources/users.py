9a567f4b514b0a658a70bb8f8f81c236b32679d2
helscn <helscns@gmail.com>
2021-06-09 20:47:03 +0800

    增加 users 资源访问接口

--- Resources/users.py
+++ Resources/users.py
@@ -1,43 +1,81 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
-from flask_restful import abort, Resource
-
-import sys 
-sys.path.append("..") 
-from models import User
 from auth import auth
+from models import User
+from flask_restful import abort, Resource, reqparse
+
+import sys
+sys.path.append("..")
+
+userParse = reqparse.RequestParser()
+userParse.add_argument('username', type=str)
+userParse.add_argument('password', type=str)
+
 
 class UserRes(Resource):
-    # decorators = [auth.login_required]
+    decorators = [auth.login_required]
+
     def get(self, id):
         user = User.get(id)
-        print('User:',user)
         if user:
-            return {'data':user.to_dict()}
+            return {'data': user.to_dict()}
         else:
-            abort(404,message="User not exist.")
+            abort(404, error='User not exist.')
 
     def delete(self, id):
         user = User.get(id)
         if user:
             user.delete()
-        return '', 204
+            return {'success': True}
+        else:
+            abort(404, error='User not exist.')
 
     def put(self, id):
         user = User.get(id)
         if user:
-            pass
-        return '', 204
+            args = userParse.parse_args()
+            username = args.get('username')
+            password = args.get('password')
+
+            if username:
+                new_user = User.query.filter(User.username == username).first()
+                if new_user and new_user.id != user.id:
+                    abort(406, error='The username already exists.')
+                else:
+                    user.username = username
+            if password:
+                if len(password) < 6:
+                    abort(406, error='The password is too short.')
+                else:
+                    user.password = password
+            user.save()
+            return {'success': True}
+        else:
+            abort(404, error='User not exist.')
+
 
 class UsersRes(Resource):
-    # decorators = [auth.login_required]
+    decorators = [auth.login_required]
+
     def get(self):
-        users=User.query.all()
+        users = User.query.all()
         return {
-            'total':len(users),
-            'data':[v.to_dict() for v in users]
+            'total': len(users),
+            'data': [v.to_dict() for v in users]
         }
 
     def post(self):
-        pass
+        args = userParse.parse_args()
+        username = args.get('username')
+        password = args.get('password')
+        if not username or not password:
+            abort(400, error='Need to provide username or password.')
+        user = User.query.filter(User.username == username).first()
+        if user:
+            abort(406, error='The username already exists.')
+        if len(password) < 6:
+            abort(406, error='The password is too short.')
+        user = User(username, password)
+        user.save()
+        return {'success': True}, 201
